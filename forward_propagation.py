05b95b9c49fff6d3a51e06da45fd0f5f5b486723
Eric Tulowetzke <[ericwolf1998@gmail.com]>
2021-03-21 19:02:40 -0500

    Updated and added .docstrings to neuron_layer.py and forward_propagation.py

--- forward_propagation.py
+++ forward_propagation.py
@@ -1,54 +1,33 @@
+"""
+- File: forward_propagation.py
+- Author: Eric Tulowetzke
+- Summary: This module contains a function for forward propagation of a artificial neural network
+"""
+
 import numpy as np
-import neuron_layer as n
+import activation_functions as a
 
 
-class ForwardNetwork:
+def forward_network(neuron_layer_array, first=True):
+    """
+    This function accepts an numpy array of instances of the neuron layer class.
+    Along with the number of layers that the ANN supposed to have.
+    It then runs through forward propagation for all layers, and updates values in the layers.
 
-    def __init__(self, layer):
-        self.layer = int(layer)
+    Args:
+        neuron_layer_array: numpy array of neuron layer instances.
 
-    def relu(self, x_array):
-        return np.maximum(x_array, 0)
-
-    def forward_pass(self, neuron_layer_array, first=True):
-        for l in range(0, self.layer):
-            if first is True:
-                input_value_t = neuron_layer_array[l].input_value.T
-                neuron_layer_array[l].z = np.dot(neuron_layer_array[l].w, input_value_t)
-                neuron_layer_array[l].a = self.relu(neuron_layer_array[l].z)
-                first = False
-            else:
-                neuron_layer_array[l].z = np.dot(neuron_layer_array[l].w, neuron_layer_array[l-1].a)
-                neuron_layer_array[l].a = self.relu(neuron_layer_array[l].z)
-        return neuron_layer_array[self.layer-1].a
-
-
-
-
-def main():
-    ann = np.array([])
-    for l in range(3):
-        tmp = n.NeuronLayer(2, 2)
-        ann = np.append(ann, tmp)
-
-    values = np.array([1, 2])
-    ann[0].input_value = np.append(ann[0].input_value, values)
-    forward = ForwardNetwork(3)
-    end = forward.forward_pass(ann)
-    for l in range(3):
-        print("layer ", l)
-        print("input")
-        print(ann[l].input_value)
-        print("w")
-        print(ann[l].w)
-        print("z")
-        print(ann[l].z)
-        print("a")
-        print(ann[l].a)
-
-    print("end, ", end)
-
-
-if __name__ == '__main__':
-    main()
+    Returns:
+         neuron_layer_array[layer-1].a: is the last output layer of the ANN"""
+    layer = int(neuron_layer_array.size)
+    for l in range(0, layer):
+        if first is True:
+            input_value_t = neuron_layer_array[l].input_value.T
+            neuron_layer_array[l].z = np.dot(neuron_layer_array[l].w, input_value_t)
+            neuron_layer_array[l].a = a.relu(neuron_layer_array[l].z)
+            first = False
+        else:
+            neuron_layer_array[l].z = np.dot(neuron_layer_array[l].w, neuron_layer_array[l-1].a)
+            neuron_layer_array[l].a = a.relu(neuron_layer_array[l].z)
+    return neuron_layer_array[layer-1].a
 
--- neuron_layer.py
+++ neuron_layer.py
@@ -1,11 +1,35 @@
+"""
+- File: neuron_layer.py
+- Author: Eric Tulowetzke
+- Summary: This module contains a class(struct) of a layer of neurons of an ANN.
+"""
 import numpy as np
 
 class NeuronLayer:
-    def __init__(self, input_dim, output_dim):
+    """
+    This class acts as a Structure that contains the information for one layer of an ANN.
+
+    Attributes:
+        input_value(1D numpy array): Holds the input values of the ANN.
+        w(m by n numpy array): This holds the values of weights for a layer in a matrix like form.
+        z(1D numpy array): Holds the values of W*x for each layer of the ANN.
+        a(1D numpy array): Holds the values of z after an activation function has been applied.
+        dCdz(1D numpy array): Is the gradient of the cost function with respect to change in z.
+        dCdw(1D numpy array): Is the gradient of the cost function with respect to change in w.
+    """
+    def __init__(self, m_dim, n_dim):
+        """
+        Constructor for class.
+        It will random Initialization of the w when created.
+
+        Args:
+              m_dim(int): number of rows in weight matrix
+              n_dim(int): number of columns in weight matrix
+
+        """
         self.input_value = np.array([])
-        self.w = np.random.rand(input_dim, output_dim)
+        self.w = np.random.rand(m_dim, n_dim)
         self.z = np.array([])
         self.a = np.array([])
-        self.grad_weight = np.array([])
-        self.grad_z = np.array([])
-        self.grad_a = np.array([])
+        self.dCdz = np.array([])
+        self.dCdw = np.array([])
