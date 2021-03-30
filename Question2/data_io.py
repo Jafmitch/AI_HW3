1d8f7b428ffb678ba96816b788ef36e1d71ee381
jafmitch <jafmitch@gmail.com>
2021-03-29 19:50:31 -0500

    Created main loop of RL. Made it so route is mapped as well as start/end/out-of-bounds

--- Question2/data_io.py
+++ Question2/data_io.py
@@ -8,24 +8,52 @@
 """
 
 import graph_wrapper as gw
-import numpy as np
+
+# Macros for map array
+OUT_OF_BOUNDS = -1
+EMPTY = 0
+START = 1
+END = 2
 
 
-def printMap(mapArray):
+def printMap(mapArray, route):
+    """
+    Prints visual of map array using graph wrapper. Will also display a route
+    taken when given a list of coordinates.
+
+    Args:
+        mapArray (list): 2d list of integers representing map of problem.
+        route (list, optional): List of coordinate lists representing the route
+                                taken. Defaults to None.
+    """
     xLen = len(mapArray)
     yLen = len(mapArray[0])
     plot = gw.Plot()
+
+    # draw lines
     for x in range(xLen):
         plot.addLine(x, yLen - 1, x, 0, color="gray")
     for y in range(yLen):
         plot.addLine(0, y, xLen - 1, y, color="gray")
+
+    # draw dots
     for x in range(xLen):
         for y in range(yLen):
-            if mapArray[x][y] == 1:
+            if mapArray[x][y] == START:
                 plot.addPoint(x, y, style="o", color="blue")
-            elif mapArray[x][y] == -1:
+            elif mapArray[x][y] == OUT_OF_BOUNDS:
                 plot.addPoint(x, y, style="o", color="red")
-            elif mapArray[x][y] == 2:
+            elif mapArray[x][y] == END:
                 plot.addPoint(x, y, style="o", color="green")
+
+    # draw route
+    X = 0
+    Y = 1
+    for i in range(len(route) - 1):
+        plot.addLine(route[i][X], route[i][Y],
+                     route[i + 1][X], route[i + 1][Y], "blue")
+
     plot.setAxis(False)
+    plot.label("Route Taken")
+    plot.save("route.jpg")
     plot.freeze()
--- Question2/hw3_2.py
+++ Question2/hw3_2.py
@@ -9,10 +9,12 @@ import graph_wrapper as gw
 
 MAP_X = 50
 MAP_Y = 50
+MAX_ITERATIONS = 10000
 
+# Macros for map array
 OUT_OF_BOUNDS = -1
 EMPTY = 0
-CURRENT = 1
+START = 1
 END = 2
 
 
@@ -20,8 +22,16 @@ def main():
     """
     The main function of this module.
     """
-    mapArray = simpleInit()
-    io.printMap(mapArray)
+    mapArray, start, end = simpleInit()
+    route = policyIterate(mapArray, start, end)
+    io.printMap(mapArray, route)
+
+
+def exitCondition(start, end):
+    X = 0
+    Y = 1
+    # replace later
+    return start[X] == end[X]
 
 
 def initMapArray():
@@ -40,19 +50,39 @@ def initMapArray():
     return mapArray
 
 
+def policyIterate(mapArray, start, end):
+    # NOTE: this is just a temporary filler until policy class gets written
+
+    route = [start]
+    # init policy
+    iteration = 0
+    while (not exitCondition([5 + iteration + 1, 25 + (iteration % 2)], end)) and iteration < MAX_ITERATIONS:
+        # copy policy into previous policy
+        # compute V
+        # choose new policy
+
+        iteration += 1
+        route.append([5 + iteration, 25 + (iteration % 2)])  # change later
+    return route
+
+
 def simpleInit():
     """
     Initializes map array with a simple out of bounds area.
 
     Returns:
         list: initialized 2d map array
+        list: starting coordinates
+        list: ending coordinates
     """
     mapArray = initMapArray()
-    mapArray[5][25] = CURRENT
+    mapArray[5][25] = START
     mapArray[45][25] = END
     for i in range(15, 30):
         mapArray[i][i] = OUT_OF_BOUNDS
-    return mapArray
+        mapArray[i][i + 1] = OUT_OF_BOUNDS
+        mapArray[i+1][i] = OUT_OF_BOUNDS
+    return mapArray, [5, 25], [45, 25]
 
 
 if __name__ == "__main__":
